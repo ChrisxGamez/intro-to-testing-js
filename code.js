fc0a05937517edae3cc94ea02343cf30192bd4ec
Christopher Gamez <ChrisxGamez@gmail.com>
2020-06-24 11:17:43 -0500

    unit tests for edge cases.

--- code.js
+++ code.js
@@ -3,25 +3,30 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello (input) {
-if (input === "Alex") {
-    return "Hello,Alex!";
+function sayHello(input) {
 
-} else if (input === "Pat") {
-        return "Hello,Pat!";
+    if (input === "Alex") {
+        return "Hello, Alex!";
 
-} else {
-    return "Hello,Jane!";
+    } else if (input === "Pat") {
+        return "Hello, Pat!";
+
+    } else if (input === "") {
+        return "Hello, World!";
+
+    } else if (input === "true") {
+        return "Hello, World!";
+
+    } else if (input === "false") {
+        return "Hello, World!";
+
+    } else if (input === null) {
+        return "Hello, World!";
+
+    } else {
+        return "Hello, Jane!";
+    }
 }
-}
-
-// function sayHello (input) {
-//     if (input === "Alex" || "Pat" || "Jane") {
-//         return "Hello, " + input + "!";
-//     }
-// }
-
-
 
 
 
